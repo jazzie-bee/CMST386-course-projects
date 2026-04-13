28671845c70128f258dc3069a830595bd541bd79
Jazzie-bee <98238715+jazzie-bee@users.noreply.github.com>
2026-04-13 18:15:19 -0500

    fixed win opening

--- project4/main.js
+++ project4/main.js
@@ -1,15 +1,16 @@
 // Open window when icon is clicked
   document.querySelectorAll('.desktop-icon').forEach(icon => {
-    icon.addEventListener('click', () => {
-      const targetId = icon.dataset.target;
-      const window = document.getElementById(targetId);
-      if (window) window.classList.add('active');
+    icon.addEventListener('click', (e) => {
+        e.preventDefault();  // stops the <a href=""> from reloading the page
+        const targetId = icon.dataset.target;
+        const win = document.getElementById(targetId);
+        if (win) win.classList.add('active');
     });
   });
 
   // Close window when X is clicked
   document.querySelectorAll('.controls').forEach(btn => {
     btn.addEventListener('click', () => {
-      btn.closest('.container').classList.remove('active');
+        btn.closest('.container').classList.remove('active');
     });
   });
