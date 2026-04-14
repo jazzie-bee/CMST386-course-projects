d57de8db9423e004adea7e722b4feee5d10225f7
Jazzie-bee <98238715+jazzie-bee@users.noreply.github.com>
2026-04-13 21:27:13 -0500

    update container classes, fix js

--- project4/main.js
+++ project4/main.js
@@ -1,10 +1,13 @@
 // Open window when icon is clicked
   document.querySelectorAll('.desktop-icon').forEach(icon => {
     icon.addEventListener('click', (e) => {
-        e.preventDefault();  // stops the <a href=""> from reloading the page
         const targetId = icon.dataset.target;
-        const win = document.getElementById(targetId);
-        if (win) win.classList.add('active');
+
+        if (targetId) {
+            e.preventDefault();  // stops the <a href=""> from reloading the page
+            const win = document.getElementById(targetId);
+            if (win) win.classList.add('active');
+        }
     });
   });
 
