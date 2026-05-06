4d55ae0043fbbddb8c7b332964f73846dbac08f6
Jazzie-bee <98238715+jazzie-bee@users.noreply.github.com>
2026-05-06 12:59:33 -0500

    accordion function js; correct naming error on commission html

--- project4/main.js
+++ project4/main.js
@@ -1,15 +1,21 @@
 // FAQ accordion
 document.querySelectorAll('.collapse-title').forEach(title => {
   title.addEventListener('click', () => {
-    const content = title.nextElementSibling;
-    const isOpen = content.style.display === 'block';
-    
-    // Close all first
-    document.querySelectorAll('.collapse-content').forEach(c => {
-      c.style.display = 'none';
-    });
+        const content = title.nextElementSibling;
+        const isOpen = content.style.display === 'block';
+        
+        // Close all first
+        document.querySelectorAll('.collapse-content').forEach(c => {
+        c.style.display = 'none';
+        });
+        document.querySelectorAll('.collapse-title').forEach(t => {
+        t.classList.remove ('open');
+        });
 
-    // Toggle clicked one
-    content.style.display = isOpen ? 'none' : 'block';
-  });
+        // Toggle clicked one
+        if (!isOpen) {
+            content.style.display = 'block';
+            title.classList.add('open');
+        }
+    });
 });
